5cee38d527d3164a16ad16ac338c9c234ca554f7
108534157 <108534157@stu.ukn.edu.tw>
2025-03-01 15:06:27 +0800

    1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Login";
-import BaccaratPrediction from "./c"; // 確保引入正確的檔案名
+import BaccaratPrediction from "./BaccaratPrediction"; // 🛠️ 確保正確的檔案名！
 import Admin from "./AdminPanel"; // ✅ 確保有引入 Admin 頁面
 
 function App() {
   return (
-    <Router>
+    <Router basename="/">
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<Navigate replace to="/login" />} />
         <Route path="/baccarat" element={<BaccaratPrediction />} />
-        <Route path="/admin" element={<Admin />} /> {/* ✅ 確保 /admin 路由存在 */}
+        <Route path="/admin" element={<Admin />} />
         <Route path="*" element={<Navigate replace to="/login" />} />
       </Routes>
     </Router>
