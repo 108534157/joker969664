2ea649f672db2bb1a69d286a8449f451f82e9166
108534157 <108534157@stu.ukn.edu.tw>
2025-03-04 17:11:28 +0800

    1

--- backend/sequelize.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-// ⚡ 使用 SQLite 資料庫
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database/database.sqlite",
-});
-
-export default sequelize;
