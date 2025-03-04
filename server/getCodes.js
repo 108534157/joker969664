b3b2628d7c7cd685a707c87a739c7445bc1c3ff5
108534157 <108534157@stu.ukn.edu.tw>
2025-03-04 15:57:03 +0800

    1

--- server/getCodes.js
+++
@@ -1,6 +0,0 @@
-import db from "../utils/db.js";
-
-export default async function handler(req, res) {
-  const result = await db.execute("SELECT * FROM codes");
-  res.status(200).json({ success: true, codes: result.rows });
-}
